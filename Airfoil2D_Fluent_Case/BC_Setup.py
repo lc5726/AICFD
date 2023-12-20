4e1062adab47490621f86c787dd38772db640dac
lc5726 <123514640+lc5726@users.noreply.github.com>
2023-12-19 21:06:06 -0500

    Added python script to change BCs

---
+++ Airfoil2D_Fluent_Case/BC_Setup.py
@@ -0,0 +1,37 @@
+import numpy as np
+import sys
+mach = float(sys.argv[1])
+AoA = float(sys.argv[2])
+Vmag = mach*np.sqrt(1.4*287.1*300)
+Vx = Vmag*np.cos(AoA)
+Vy = Vmag*np.sin(AoA)
+
+Script = '''/file/set-tui-version "20.2"
+
+/file/read-case results/Airfoil2d_db.cas
+/define/boundary-conditions/set/velocity-inlet inlet () velocity-spec no yes quit 
+/define/boundary-conditions/set/velocity-inlet inlet () direction-0 no %.2f quit 
+/define/boundary-conditions/set/velocity-inlet inlet () direction-1 no %.2f quit 
+/solve/initialize/initialize-flow 
+/solve/iterate 1000
+
+/solve/cell-registers/add "airfoil" quit
+/solve/cell-registers/edit "airfoil" type boundary boundary-list airfoil () quit quit
+/mesh/modify-zones/ sep-cell-zone-mark fluid-part1 airfoil yes quit
+/file/export/ascii airfoil_bc_%.2f_%.2f interior-part1:002 () no x-velocity () yes quit
+
+/solve/cell-registers/add "inlet" quit
+/solve/cell-registers/edit "inlet" type boundary boundary-list inlet () quit quit
+/mesh/modify-zones/ sep-cell-zone-mark fluid-part1 inlet yes quit
+/file/export/ascii inlet_bc_%.2f_%.2f interior-part1:017 () no x-velocity () yes quit
+
+/solve/cell-registers/add "outlet" quit
+/solve/cell-registers/edit "outlet" type boundary boundary-list outlet () quit quit
+/mesh/modify-zones/ sep-cell-zone-mark fluid-part1 outlet yes quit
+/file/export/ascii outlet_bc_%.2f_%.2f interior-part1:023 () no x-velocity () yes quit
+
+/exit'''%(Vx,Vy,AoA,mach,AoA,mach,AoA,mach)
+
+with open('fluent_run.jou','w') as o:
+    o.write(Script)
+
